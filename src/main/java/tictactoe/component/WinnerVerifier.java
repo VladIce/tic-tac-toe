77cb0c90c9a43a14637812192f991607595f7d16
Vladislav ilin <nilindalv@yandex.ru>
2023-08-22 22:39:38 +0300

    Implement WinnerVerifier component

--- src/main/java/tictactoe/component/WinnerVerifier.java
+++ src/main/java/tictactoe/component/WinnerVerifier.java
@@ -16,14 +16,57 @@
 
 package tictactoe.component;
 
+import tictactoe.model.Cell;
 import tictactoe.model.GameTable;
 
 public class WinnerVerifier {
     public boolean isUserWin(GameTable gameTable) {
-        return false;
+        return isWinner(gameTable, 'X');
     }
 
     public boolean isComputerWin(GameTable gameTable) {
+        return isWinner(gameTable, '0');
+    }
+
+    private boolean isWinner(GameTable gameTable, char sign) {
+        return isWinnerByRows(gameTable, sign) ||
+                isWinnerByCols(gameTable, sign) ||
+                isWinnerByMainDiagonal(gameTable, sign) ||
+                isWinnerBySecondaryDiagonal(gameTable, sign);
+    }
+
+    private boolean isWinnerBySecondaryDiagonal(GameTable gameTable, char sign) {
+        return gameTable.getSign(new Cell(2, 0)) == gameTable.getSign(new Cell(1, 1)) &&
+                gameTable.getSign(new Cell(1, 1)) == gameTable.getSign(new Cell(0, 2)) &&
+                gameTable.getSign(new Cell(0, 2)) == sign;
+    }
+
+    private boolean isWinnerByMainDiagonal(GameTable gameTable, char sign) {
+        return gameTable.getSign(new Cell(0, 0)) == gameTable.getSign(new Cell(1, 1)) &&
+                gameTable.getSign(new Cell(1, 1)) == gameTable.getSign(new Cell(0, 2)) &&
+                gameTable.getSign(new Cell(2, 2)) == sign;
+
+    }
+
+    private boolean isWinnerByCols(GameTable gameTable, char sign) {
+        for (int i = 0; i < 3; i++) {
+            if (gameTable.getSign(new Cell(0, i)) == gameTable.getSign(new Cell(1, i)) &&
+                    gameTable.getSign(new Cell(1, i)) == gameTable.getSign(new Cell(2, i)) &&
+                    gameTable.getSign(new Cell(2, i)) == sign) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isWinnerByRows(GameTable gameTable, char sign) {
+        for (int i = 0; i < 3; i++) {
+            if (gameTable.getSign(new Cell(i, 0)) == gameTable.getSign(new Cell(i, 1)) &&
+                    gameTable.getSign(new Cell(i, 1)) == gameTable.getSign(new Cell(i, 2)) &&
+                    gameTable.getSign(new Cell(i, 2)) == sign) {
+                return true;
+            }
+        }
         return false;
     }
 }
